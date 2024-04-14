7873062b1d1118d135e8b70c7f550dc719b3b655
Andrew Fei <andrew77498864@gmail.com>
2024-04-15 01:56:46 +0800

    更新登入後首頁的 UI 及資訊

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,28 @@
 @extends('layouts.default')
 
 @section('main')
-    <h1>首頁</h1>
-    <h2>姓名：{{ $user->name }}</h2>
-    <h2>帳號：{{ $user->account }}</h2>
-    <h2>Email：{{ $user->email }}</h2>
+    <h1 class="text-3xl font-bold mb-6">首頁</h1>
 
-    @if ($user->status === 'pending')
-        <p>此帳號待審核開通</p>
-    @endif
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div class="space-y-2">
+            <h2 class="text-lg font-medium">姓名：{{ $user->name }}</h2>
+            <h2 class="text-lg font-medium">帳號：{{ $user->account }}</h2>
+            <h2 class="text-lg font-medium">Email：{{ $user->email }}</h2>
+            <h2 class="text-lg font-medium">機關單位代碼：{{ $user->organization->org_no }}</h2>
+            <h2 class="text-lg font-medium">機關單位名稱：{{ $user->organization->title }}</h2>
+        </div>
 
-    <form method="POST" action="{{ url('/sessions') }}" class="inline-block">
-        @method('DELETE')
-        @csrf
-        <button class="btn btn-red" type="submit">登出</button>
-    </form>
+        <div class="space-y-2">
+            @if ($user->status === 'pending')
+                <p class="text-red-600">此帳號待審核開通</p>
+            @endif
+
+            <form method="POST" action="{{ url('/sessions') }}" class="inline-block">
+                @method('DELETE')
+                @csrf
+                <button class="bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded" type="submit">登出</button>
+            </form>
+        </div>
+    </div>
 
 @endsection
